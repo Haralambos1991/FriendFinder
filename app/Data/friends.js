3477a73adaa250364fd19b93232906c5d8d41384
Haralambos <lampistsivikas@yahoo.com>
2019-01-04 10:03:24 -0800

    message

---
+++ app/Data/friends.js
@@ -0,0 +1,147 @@
+var friendsArray = [
+    {
+    name: "Kate Hudson",
+    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwigmPzczKrfAhWEMX0KHSlYB_kQjRx6BAgBEAU&url=https%3A%2F%2Fgiphy.com%2Fgifs%2Falmost-famous-UXcMHOwyR0cbm&psig=AOvVaw1s2l5-Pe5mmqzYnjegApfa&ust=1545264166750536",
+    scores: [
+    "4",
+    "3",
+    "1",
+    "2",
+    "2",
+    "5",
+    "4",
+    "2",
+    "5",
+    "3"
+    ]
+    },
+    {
+     name: "Kate Moss",
+     photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjX-of4zarfAhUHGDQIHTicCPsQjRx6BAgBEAU&url=https%3A%2F%2Fperezhilton.com%2F2014-07-30-kate-moss-drunk-at-airport-without-plane-ticket%2F&psig=AOvVaw1rc7jLRoM1m6AS8-tGfSGx&ust=1545264750537803",
+     scores: [
+    "5",
+    "2",
+    "3",
+    "4",
+    "4",
+    "1",
+    "3",
+    "4",
+    "5",
+    "3"
+     ]
+    },
+    {
+    name: "Brad Pitt",
+    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiI_4ujz6rfAhUYFzQIHbKUDu4QjRx6BAgBEAU&url=https%3A%2F%2Fgiphy.com%2Fgifs%2Fbrad-pitt-fight-club-11LCNfhGiwgD0A&psig=AOvVaw1yY37J2ebOVq921vLEZPX-&ust=1545265109764928",
+    scores: [
+    "3",
+    "2",
+    "1",
+    "5",
+    "4",
+    "2",
+    "3",
+    "2",
+    "3",
+    "5"
+    ]
+    },
+    {
+     name: "Angelina Jolie",
+     photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi20_DC0KrfAhUaITQIHcp3Ap4QjRx6BAgBEAU&url=https%3A%2F%2Fgiphy.com%2Fgifs%2Fbaby-kids-angelina-jolie-AELXQApXhzwvC&psig=AOvVaw2JuwVfypPrqLHVz05UF7ae&ust=1545265432044920",
+     scores: [
+      "5",
+      "3",
+      "4",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "4"
+     ]
+    },
+    {
+    name: "Jennifer Aniston",
+    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiO29ao0arfAhWsGDQIHUhUBGIQjRx6BAgBEAU&url=https%3A%2F%2Fgiphy.com%2Fgifs%2Fsmile-jennifer-aniston-jen-3s7pl5CPNpdN6&psig=AOvVaw1vyS67Zb2pvCszUrT9repY&ust=1545265608204228",
+    scores: [
+      "3",
+      "5",
+      "2",
+      "5",
+      "4",
+      "3",
+      "4",
+      "3",
+      "4",
+      "3"
+    ]
+    },
+    {
+    name: "Emma Stone",
+    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiXtsKR1KrfAhV8ITQIHd-bAVwQjRx6BAgBEAU&url=https%3A%2F%2Fgiphy.com%2Fgifs%2Femma-stone-smiling-oh-boy-tWrXaAUjD7ujm&psig=AOvVaw2MysOIYkiM4wgSqcs-GJK9&ust=1545266390080636",
+    scores: [
+    "3",
+    "5",
+    "2",
+    "5",
+    "2",
+    "5",
+    "3",
+    "5",
+    "3",
+    "4"
+    ]
+    },
+    {
+     name: "Emmy Smart"  ,
+     photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiQ3NiK1arfAhUmJjQIHRKSCDYQjRx6BAgBEAU&url=https%3A%2F%2Fgifer.com%2Fen%2FKZBc&psig=AOvVaw2j-BogwmbmP8hmVL99hvzt&ust=1545266646896604",
+     scores: [
+        "5",
+        "3",
+        "2",
+        "5",
+        "2",
+        "2",
+        "3",
+        "5",
+        "3",
+        "4"
+     ]
+    },
+    {
+    name: "Rachel Mac Adams",
+    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjH2bWj1qrfAhWJHTQIHU8iAHAQjRx6BAgBEAU&url=https%3A%2F%2Fgifer.com%2Fen%2F3RAJ&psig=AOvVaw1bZL8wsyfZbAcuvlvBAzLW&ust=1545266981062853",
+    scores: [
+        "3",
+        "4",
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "5"
+    ]
+    },
+    {
+        name: "Margot Robbie",
+        photo: "https://media.giphy.com/media/ov44v68NlxQgE/giphy.gif",
+        scores: [
+            "5",
+            "4",
+            "4",
+            "2",
+            "2",
+            "4",
+            "2",
+            "5",
+            "4",
+            "5"
+        ]
+    }
+    ];
+    // Right now i am gonna export trhe array//
+    module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,22 @@
+//Here se define our dependencies//
+
+var express = require("express");
+var path = require("path");
+
+var app = express();
+//Here i am gonna set the port of my application//
+var PORT = process.env.Port|| 3000;
+
+//Below we will use the bodyparser in order to interpret data to the server//
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//When the user requests data from various url, this code will help him find the informations that the user needs.
+
+require('./app/routing/apiRoutes.js') (app);
+require('./app/routing/htmlRoutes.js') (app);
+
+//We will start our server//
+
+app.listen(PORT, function() {
+console.log("app is listening on PORT:" + PORT);
+});
