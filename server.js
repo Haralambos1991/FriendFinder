7cb94f821b6ddb7fe9223a50e239d0807cab7037
Haralambos <lampistsivikas@yahoo.com>
2019-08-31 14:59:22 -0700

    new message

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ var PORT = process.env.Port|| 3000;
 //Below we will use the bodyparser in order to interpret data to the server//
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//When the user requests data from various url, this code will help him find the informations that the user needs.
+// //When the user requests data from various url, this code will help him find the informations that the user needs.
 
 require('./app/routing/apiRoutes.js') (app);
 require('./app/routing/htmlRoutes.js') (app);
 
-//We will start our server//
+// //We will start our server//
 
 app.listen(PORT, function() {
 console.log("app is listening on PORT:" + PORT);
 });
+
