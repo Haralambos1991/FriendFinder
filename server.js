545620f3f41838bd31a681cea0805b29acebdd01
Haralambos <lampistsivikas@yahoo.com>
2019-09-10 10:49:45 -0700

    new order

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 var PORT = process.env.Port|| 3000;
 
 //Below we will use the bodyparser in order to interpret data to the server//
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(_dirname+'/public',{dotfiles:'allow'}));
 app.use(express.json());
 // //When the user requests data from various url, this code will help him find the informations that the user needs.
 
