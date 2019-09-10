3fd941df6575ec945883952a6a47ec2dde9a380b
Haralambos <lampistsivikas@yahoo.com>
2019-09-10 10:31:05 -0700

    new message

--- app/Routing/apiRoutes.js
+++ app/Routing/apiRoutes.js
@@ -15,3 +15,33 @@ var scoresArray = [];
 var FriendCount = 0;
 var bestMatch = 0;
 
+//runs through all current friends in list //
+
+for (var i = 0; i < FriendsList.length; i++){
+	var scoresDiff = 0;
+
+// runs through the scores //
+
+for (var j = 0; j < newFriendScores.length; j++){
+	scoresDiff += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
+}
+
+//push results into scoresArray
+scoresArray.push(scoresDiff);
+}
+
+//after all friends are compared, find best match
+for(var i=0; i<scoresArray.length; i++){
+if(scoresArray[i] <= scoresArray[bestMatch]){
+  bestMatch = i;
+}
+}
+
+//return bestMatch data
+var bff = friendList[bestMatch];
+res.json(bff);
+
+//pushes new submission into the friendsList array
+friendList.push(req.body);
+});
+};	
